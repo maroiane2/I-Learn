09f30488301325112fe0cb81ce90fd9c5adf6aed
marouan <devopscode9@gmail.com>
2023-02-08 16:43:35 +0100

    edite in json

--- _API/script.js
+++ _API/script.js
@@ -1,10 +1,20 @@
 fetch("https://randomuser.me/api/")
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data)
     })
 
 
 let content = document.getElementById("content")
+
+
+
+
+var b1 = {
+    "name": "marouane",
+    "age": 18
+};
+
+console.log(b1);
